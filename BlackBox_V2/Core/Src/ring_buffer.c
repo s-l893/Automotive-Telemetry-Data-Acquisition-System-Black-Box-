ab72113c299e8bb47e9cfaf80a0713a1e9a3327a
s-l893 <sunnylin893@gmail.com>
2026-07-08 19:15:28 -0400

    added circular buffer architecture .c file

--- BlackBox_V2/Core/Src/ring_buffer.c
+++ BlackBox_V2/Core/Src/ring_buffer.c
@@ -5,4 +5,48 @@
  *      Author: Sunny Lin
  */
 
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+	uint8_t *buffer;
+	uint16_t head;
+	uint16_t tail;
+	uint16_t count;
+	uint16_t capacity;
+	uint16_t dropped_count;
+} ring_buffer_t;
+
+
+void RingBuffer_Init(ring_buffer_t *rb, uint16_t capacity, uint8_t *buffer){
+	rb->head = 0;
+	rb->tail = 0;
+	rb->count = 0;
+	rb->dropped_count = 0;
+	rb->buffer = buffer;
+	rb->capacity = capacity;
+}
+bool RingBuffer_Push(ring_buffer_t *rb, uint8_t data){
+	rb -> buffer[rb->head] = data;
+	rb->head = (rb->head + 1) % rb->capacity;
+	if (rb->count == rb->capacity){
+		rb -> tail = (rb -> tail + 1) % rb->capacity;
+		rb->dropped_count++;
+	}
+	else{
+		rb->count++;
+	}
+	return true;
+}
+bool RingBuffer_Pop(ring_buffer_t *rb, uint8_t *data_out){
+	if (rb->count == 0){
+		return false;
+	}
+	*data_out = rb->buffer[rb->tail];
+	rb -> tail = (rb->tail + 1) % rb->capacity;
+	rb -> count--;
+	return true;
+}
+
+
 
