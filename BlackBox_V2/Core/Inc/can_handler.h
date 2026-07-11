90ae225bc625115dba4f66937eaf694fb264b4b4
s-l893 <sunnylin893@gmail.com>
2026-07-11 01:27:28 -0400

    canhandler header complete

--- BlackBox_V2/Core/Inc/can_handler.h
+++ BlackBox_V2/Core/Inc/can_handler.h
@@ -8,6 +8,14 @@
 #ifndef INC_CAN_HANDLER_H_
 #define INC_CAN_HANDLER_H_
 
+#include "can_ring_buffer.h"
+#include <stdbool.h>
+
+extern volatile can_ring_buffer_t can_rb;
+extern volatile bool can_frame_received_flag;
+extern volatile uint32_t last_can_frame;
+
+void can_handler_init(void);
 
 
 #endif /* INC_CAN_HANDLER_H_ */
