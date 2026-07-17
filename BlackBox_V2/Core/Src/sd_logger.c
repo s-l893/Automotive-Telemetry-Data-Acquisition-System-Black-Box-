08cc32c4c43bf19eac29654a586cd79affecf8fe
s-l893 <sunnylin893@gmail.com>
2026-07-17 02:11:41 -0400

    unfinished can_rb flush to sd card logic

--- BlackBox_V2/Core/Src/sd_logger.c
+++ BlackBox_V2/Core/Src/sd_logger.c
@@ -9,11 +9,14 @@
 #include "fatfs.h"
 #include <stdbool.h>
 #include "main.h"
+#include "can_handler.h"
 
 FATFS fs;
 FIL log_file;
 
 char filename[32];
+
+
 
 // #include "gps.c"
 
@@ -57,5 +60,31 @@ void sd_recovery(void) {
 void unmount_sd(void){
 	f_mount(NULL, "", 0);
 	sd_mount = false;
+}
+
+void SD_Logger_DrainCAN(void){
+	can_frame_t frame;
+	int offset = 0;
+	uint16_t bytes_written = 0;
+	char csv_buffer[1024];
+
+	for (int i = 0; i < 16; i++){
+		bool got_frame = CANRingBuffer_Pop(&can_rb, &frame);
+		if(!got_frame){
+			break;
+		}
+
+		snprintf(csv_buffer + offset, sizeof(csv_buffer), "%lu,0x%031X,%d\n", frame.timestamp, frame.id, frame.dlc);
+		int written = snprintf(csv_buffer + offset, sizeof(csv_buffer) - offset, "%lu,0x%03lX,%d\n", frame.timestamp, frame.id, frame.dlc, frame.data);
+		offset += written;
+	}
+	if (offset > 0){
+		break;
+	}
+
+	FRESULT verify = f_write(&log_file, csv_buffer, offset, &bytes_written);
+	if (verify != FR_OK){
+		fault_flags.sd_fault = true;
+	}
 }
 
