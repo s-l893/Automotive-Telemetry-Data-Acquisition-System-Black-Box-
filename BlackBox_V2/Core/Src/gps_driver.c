84ee195b7859bed88071051e6071b0284260ccba
s-l893 <sunnylin893@gmail.com>
2026-09-03 03:43:09 -0400

    gps driver finished, currently untested and integrated into rest of the stack

--- BlackBox_V2/Core/Src/gps_driver.c
+++ BlackBox_V2/Core/Src/gps_driver.c
@@ -8,6 +8,12 @@
 #include "main.h"
 #include <stdbool.h>
 
+char nmea_buffer[85];
+char nmea_parse_buffer[85];
+
+volatile uint16_t nmea_index = 0;
+volatile bool nmea_line_ready = false;
+static uint8_t rx_byte;
 
 typedef struct {
 	bool locked;
@@ -23,10 +29,88 @@ void GPS_Driver_Init(void){
 	gps.speed = 0.0;
 	gps.latitude = 0.0;
 	gps.longitude = 0.0;
+	nmea_index = 0;
+	HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
 }
 
 void GPS_Driver_Update(void){
-	if (gps.locked){
+	if (nmea_line_ready){
+		rmc_splitter();
+	}
+}
+
+float parse_lat_long(char* field, char hemisphere, int degree_digits){
+	char deg_str[4]; // 3 DIGITS + TERMINATOR
+	strncpy(deg_str, field, degree_digits);
+	deg_str[degree_digits] = '\0';
+
+	float degrees = atof (deg_str); // convert to float
+	float minutes = atof(fields + degree_digits);
+
+	float decimal = degrees + (minutes / 60.0); // formula to convert degrees and minutes into decimal degree
+
+	if (hemisphere == 'S' || hemisphere == 'W'){ // prevent (-)s
+		decimal = -decimal;
+	}
+	return decimal;
+}
+
+void rmc_splitter(void){
+	char *status;
+	char *valid = "A";
+//	float time;
+	char *lat_hemi;
+	char *long_hemi;
+	char *lat_field;
+	char *long_field;
+	char *date;
+	float speed;
+
+	strtok(nmea_parse_buffer, ","); // FIELD 0
+	//time = strtok(NULL, ","); //FIELD 1 (time)
+	status = strtok(NULL, ","); // FIELD 2 (STATUS)
+	if (!(strcmp(status, valid))){
+		gps.locked = true;
+
+		lat_field = strtok(NULL, ','); // FIELD 3 (LAT)
+		lat_hemi = strtok(NULL, ','); // FIELD 4 (LAT HEMI)
+		float latitude = parse_lat_long(lat_field,lat_hemi[0], 2);
+
+		long_field = strtok (NULL, ','); // FIELD 5 (LONG)
+		long_hemi = strtok(NULL, ','); // FIELD 6 (LONG HEMI)
+		float longitude = parse_lat_long (long_field, long_hemi[0], 3);
+
+		gps.latitude = latitude; // UPDATE STRUCT
+		gps.longitude = longitude;
+
+		speed = atof(strtok(NULL, ',')); // FIELD 7
+		speed = speed * 1.852; // KNOTS TO KM/H
+		gps.speed = speed;
+
+		strtok(NULL, ','); // FIELD 8
+		date = strtok(NULL, ','); // FIELD 9
 
 	}
+	else{
+		gps.locked = false;
+	}
+	nmea_line_ready = false;
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	if (nmea_index < 85){ // prevent buffer overflow
+		if (rx_byte == '\n'){ // check for end of bytes
+			nmea_buffer[nmea_index] = '\0'; // manually add NTERMINATOR
+			memcpy(nmea_parse_buffer,nmea_buffer, nmea_index + 1); // +1 for terminator
+			nmea_line_ready = true;
+			nmea_index = 0; // reset buffer w index
+
+		}
+		else{
+			nmea_buffer[nmea_index] = rx_byte;
+			nmea_index++;
+		}
+	}
+
+	HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
 }
--- BlackBox_V2/Core/Src/imu.c
+++ BlackBox_V2/Core/Src/imu.c
@@ -1,82 +1,164 @@
-/*
- * imu.c
- *
- *  Created on: Aug 18, 2026
- *      Author: Sunny Lin
- */
-
-#include "imu.h"
-#include "fault.h"
-#include "i2c.h"
-#include "iwdg.h"
-#include <stdbool.h>
-#include <stdint.h>
-#include "main.h"
-
-#define IMU_ADDR 0x68
-#define WAKE_REG 0x6B
-#define WRITE_ADDRESS (IMU_ADDR << 1)
-
-imu_frame imu;
-imu_calibration imu_offset;
-uint8_t imu_who_am_i = 0;
-HAL_StatusTypeDef imu_who_read_status = HAL_ERROR;
-HAL_StatusTypeDef imu_wake_write_status = HAL_ERROR;
-HAL_StatusTypeDef imu_accel_read_status = HAL_ERROR;
-
-void imu_read(void){
-	if (!fault_flags.imu_fault && !fault_flags.imu_handshake_fault ){
-		uint8_t buffer[6];
-		imu_accel_read_status = HAL_I2C_Mem_Read(&hi2c1, WRITE_ADDRESS, 59, I2C_MEMADD_SIZE_8BIT, buffer, 6, 100);
-		if (imu_accel_read_status != HAL_OK){
-			fault_flags.imu_fault = true;
-			return;
-		}
-		imu.accel_x = (int16_t)((buffer[0] << 8) | buffer[1]);
-		imu.accel_y = (int16_t)((buffer[2] << 8) | buffer[3]);
-		imu.accel_z = (int16_t)((buffer[4] << 8) | buffer[5]);
-		imu.accel_x -= imu_offset.offset_x;
-		imu.accel_y -= imu_offset.offset_y;
-		imu.accel_z -= imu_offset.offset_z;
-		imu.timestamp = HAL_GetTick();
-	}
-}
-
-void imu_calibrate(void){ // ZERO CALIBRATION UPON START
-	int32_t sum_x = 0;
-	int32_t sum_y = 0;
-	int32_t sum_z = 0;
-	if ((fault_flags.imu_fault == false) && (fault_flags.imu_handshake_fault == 0)){
-
-		for (int i = 0; i < 50; i++){
-			imu_read();
-			sum_x += imu.accel_x;
-			sum_y += imu.accel_y;
-			sum_z += imu.accel_z;
-			HAL_IWDG_Refresh(&hiwdg);
-			HAL_Delay(20);
-		}
-
-		imu_offset.offset_x = (int16_t)(sum_x / 50);
-		imu_offset.offset_y = (int16_t)(sum_y / 50);
-		imu_offset.offset_z = (int16_t)(sum_z / 50);
-	}
-}
-
-void imu_init(void){
-
-	uint8_t sleep_bit = 0x00;
-	imu_who_am_i = 0;
-	imu_wake_write_status = HAL_I2C_Mem_Write(&hi2c1, WRITE_ADDRESS, WAKE_REG, I2C_MEMADD_SIZE_8BIT, &sleep_bit, 1, 100);
-	if (imu_wake_write_status != HAL_OK){
-		fault_flags.imu_fault = true;
-	}
-	imu_who_read_status = HAL_I2C_Mem_Read(&hi2c1, WRITE_ADDRESS, 117, I2C_MEMADD_SIZE_8BIT, &imu_who_am_i, 1, 100);
-	if (imu_who_read_status != HAL_OK){
-		fault_flags.imu_fault = true;
-	}
-	if (imu_who_am_i != 0x68){
-		fault_flags.imu_handshake_fault = true;
-	}
-	imu_calibrate();
-}
+/*
+
+ * imu.c
+
+ *
+
+ *  Created on: Aug 18, 2026
+
+ *      Author: Sunny Lin
+
+ */
+
+
+
+#include "imu.h"
+
+#include "fault.h"
+
+#include "i2c.h"
+
+#include "iwdg.h"
+
+#include <stdbool.h>
+
+#include <stdint.h>
+
+#include "main.h"
+
+
+
+#define IMU_ADDR 0x68
+
+#define WAKE_REG 0x6B
+
+#define WRITE_ADDRESS (IMU_ADDR << 1)
+
+
+
+imu_frame imu;
+
+imu_calibration imu_offset;
+
+uint8_t imu_who_am_i = 0;
+
+HAL_StatusTypeDef imu_who_read_status = HAL_ERROR;
+
+HAL_StatusTypeDef imu_wake_write_status = HAL_ERROR;
+
+HAL_StatusTypeDef imu_accel_read_status = HAL_ERROR;
+
+
+
+void imu_read(void){
+
+	if (!fault_flags.imu_fault && !fault_flags.imu_handshake_fault ){
+
+		uint8_t buffer[6];
+
+		imu_accel_read_status = HAL_I2C_Mem_Read(&hi2c1, WRITE_ADDRESS, 59, I2C_MEMADD_SIZE_8BIT, buffer, 6, 100);
+
+		if (imu_accel_read_status != HAL_OK){
+
+			fault_flags.imu_fault = true;
+
+			return;
+
+		}
+
+		imu.accel_x = (int16_t)((buffer[0] << 8) | buffer[1]);
+
+		imu.accel_y = (int16_t)((buffer[2] << 8) | buffer[3]);
+
+		imu.accel_z = (int16_t)((buffer[4] << 8) | buffer[5]);
+
+		imu.accel_x -= imu_offset.offset_x;
+
+		imu.accel_y -= imu_offset.offset_y;
+
+		imu.accel_z -= imu_offset.offset_z;
+
+		imu.timestamp = HAL_GetTick();
+
+	}
+
+}
+
+
+
+void imu_calibrate(void){ // ZERO CALIBRATION UPON START
+
+	int32_t sum_x = 0;
+
+	int32_t sum_y = 0;
+
+	int32_t sum_z = 0;
+
+	if ((fault_flags.imu_fault == false) && (fault_flags.imu_handshake_fault == 0)){
+
+
+
+		for (int i = 0; i < 50; i++){
+
+			imu_read();
+
+			sum_x += imu.accel_x;
+
+			sum_y += imu.accel_y;
+
+			sum_z += imu.accel_z;
+
+			HAL_IWDG_Refresh(&hiwdg);
+
+			HAL_Delay(20);
+
+		}
+
+
+
+		imu_offset.offset_x = (int16_t)(sum_x / 50);
+
+		imu_offset.offset_y = (int16_t)(sum_y / 50);
+
+		imu_offset.offset_z = (int16_t)(sum_z / 50);
+
+	}
+
+}
+
+
+
+void imu_init(void){
+
+
+
+	uint8_t sleep_bit = 0x00;
+
+	imu_who_am_i = 0;
+
+	imu_wake_write_status = HAL_I2C_Mem_Write(&hi2c1, WRITE_ADDRESS, WAKE_REG, I2C_MEMADD_SIZE_8BIT, &sleep_bit, 1, 100);
+
+	if (imu_wake_write_status != HAL_OK){
+
+		fault_flags.imu_fault = true;
+
+	}
+
+	imu_who_read_status = HAL_I2C_Mem_Read(&hi2c1, WRITE_ADDRESS, 117, I2C_MEMADD_SIZE_8BIT, &imu_who_am_i, 1, 100);
+
+	if (imu_who_read_status != HAL_OK){
+
+		fault_flags.imu_fault = true;
+
+	}
+
+	if (imu_who_am_i != 0x68){
+
+		fault_flags.imu_handshake_fault = true;
+
+	}
+
+	imu_calibrate();
+
+}
+
