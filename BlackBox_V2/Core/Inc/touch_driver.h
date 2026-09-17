89c57e71dc11c9560c220b6999d7095b8190c45e
s-l893 <sunnylin893@gmail.com>
2026-09-17 00:55:36 -0400

    touch driver nearing completion

--- BlackBox_V2/Core/Inc/touch_driver.h
+++ BlackBox_V2/Core/Inc/touch_driver.h
@@ -8,6 +8,35 @@
 #ifndef TARGET_TOUCH_DRIVER_H_
 #define TARGET_TOUCH_DRIVER_H_
 
+void Touch_ToPixel(uint16_t raw_x, uint16_t raw_y, uint16_t *pixel_x, uint16_t *pixel_y);
+void Touch_CS_High(void);
+void Touch_CS_Low(void);
+void Touch_ReadRaw(uint16_t *raw_x, uint16_t *raw_y);
+void Touch_Init(void);
 
+typedef enum {
+    TOUCH_NONE,
+    TOUCH_TAP,
+    TOUCH_SWIPE
+} touch_gesture_t;
 
+typedef enum {
+    SWIPE_LEFT,
+    SWIPE_RIGHT
+} swipe_dir_t;
+
+typedef struct {
+    bool gesture_ready;
+    touch_gesture_t type;
+    union {
+        struct {
+            uint16_t pixel_x;
+            uint16_t pixel_y;
+        } tap;
+        swipe_dir_t swipe_direction;
+    };
+} touch_data_t;
+
+extern touch_data_t touch;
 #endif /* TARGET_TOUCH_DRIVER_H_ */
+
--- BlackBox_V2/Core/Src/touch_driver.c
+++ BlackBox_V2/Core/Src/touch_driver.c
@@ -7,6 +7,8 @@
 
 #include "touch_driver.h"
 #include "main.h"
+#include "fault.h"
+#include <math.h>
 
 #include <stdbool.h>
 
@@ -20,6 +22,16 @@
 #define pixely2 0
 
 static volatile bool touch_event_flag = false;
+static volatile bool touch_in_progress = false;
+static volatile uint32_t touch_timer = 0;
+static uint16_t touch_start_raw_x;
+static uint16_t touch_start_raw_y;
+
+static uint16_t touch_latest_raw_x;
+static uint16_t touch_latest_raw_y;
+
+static int16_t touch_total_x;
+static int16_t touch_total_y;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO){
 	if (GPIO == TOUCH_IRQ_Pin){
@@ -39,8 +51,71 @@ void Touch_ToPixel(uint16_t raw_x, uint16_t raw_y, uint16_t *pixel_x, uint16_t *
 	*pixel_y = m_y * raw_y + b_y;
 }
 
+void Touch_CS_High(void){
+	HAL_GPIO_WritePin(CS_SPI2_Port, CS_SPI2_Pin, GPIO_PIN_SET);
+}
+
+void Touch_CS_Low(void){
+	HAL_GPIO_WritePin(CS_SPI2_Port, CS_SPI2_Pin, GPIO_PIN_RESET);
+}
+
 void Touch_ReadRaw(uint16_t *raw_x, uint16_t *raw_y){
-	uint16_t tx = {control_byte, 0, 0};
-	uint16_t rx = {;
-	HAL_SPI_TransmitReceive(&hspi2, *tx, *rx, 1, HAL_MAX_DELAY);
+	uint8_t control_byte_x = 0xD0;
+	uint8_t control_byte_y = 0x90;
+
+	Touch_CS_Low();
+
+	uint8_t tx[3] = {control_byte_x, 0, 0};
+	uint8_t rx[3];
+	HAL_SPI_TransmitReceive(&hspi2, tx, rx, 3, HAL_MAX_DELAY);
+	uint16_t raw_value_x = (rx[1] << 8) | (rx[2] >> 4);
+	Touch_CS_High();
+
+	Touch_CS_Low();
+	tx[0] = control_byte_y;
+	HAL_SPI_TransmitReceive(&hspi2, tx, rx, 3, HAL_MAX_DELAY);
+	uint16_t raw_value_y = (rx[1] << 8) | (rx[2] >> 4);
+	Touch_CS_High();
+
+	*raw_x = raw_value_x;
+	*raw_y = raw_value_y;
+
+}
+
+void Touch_Init(void){
+	Touch_CS_High();
+	touch_event_flag = false;
+
+}
+
+
+
+void Touch_Update(void){
+
+	if(!touch_in_progress){
+		if (touch_event_flag){
+			touch_timer = HAL_GetTick();
+			touch_in_progress = true;
+
+			Touch_ReadRaw(&touch_start_raw_x, &touch_start_raw_y);
+			touch_event_flag = false;
+		}
+	}
+	else{
+
+		if(HAL_GPIO_ReadPin(TOUCH_IRQ_GPIO_PORT, TOUCH_IRQ_PIN) == GPIO_PIN_RESET){
+			Touch_ReadRaw(&touch_latest_raw_x, &touch_latest_raw_y);
+
+		}
+		else{
+			Touch_ReadRaw(&touch_latest_raw_x, &touch_latest_raw_y);
+			touch_in_progress = false;
+			touch_total_x = touch_latest_raw_x - touch_start_raw_x;
+			touch_total_y = touch_latest_raw_y - touch_start_raw_y;
+		}
+		if ((HAL_GetTick() - touch_timer) > 12000){
+			touch_in_progress = false;
+			fault_flags.touch_fault = true;
+		}
+	}
 }
